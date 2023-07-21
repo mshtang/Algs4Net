912f756a0a4c37d19ebdd02ba67595b657dfe3f7
mshtang <32868278+mshtang@users.noreply.github.com>
2023-07-21 19:24:17 +0200

    Finish 1.1.11

---
+++ SolutionTests/Chapter1/Section1/S11Tests.cs
@@ -0,0 +1,18 @@
+namespace SolutionTests.Chapter1.Section1;
+using Solutions.Chapter1.Section1;
+
+[TestClass]
+public class S11Tests
+{
+    [TestMethod]
+    public void TransposeTests()
+    {
+        var mat = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+        var res = S11.Transpose(mat);
+        Assert.IsTrue(res.GetUpperBound(0) + 1 == 3);
+        Assert.IsTrue(res.GetUpperBound(1) + 1 == 2);
+        Assert.IsTrue(res[0, 1] == mat[1, 0]);
+        Assert.IsTrue(res[1, 0] == mat[0, 1]);
+        Assert.IsTrue(res[1, 1] == mat[1, 1]);
+    }
+}
---
+++ Solutions/Chapter1/Section1/S11.cs
@@ -0,0 +1,21 @@
+namespace Solutions.Chapter1.Section1;
+
+public static class S11
+{
+    public static int[,] Transpose(int[,] mat)
+    {
+        var rows = mat.GetUpperBound(0) + 1;
+        var cols = mat.GetUpperBound(1) + 1;
+
+        var res = new int[cols, rows];
+
+        for (var r = 0; r < cols; r++)
+        {
+            for (var c = 0; c < rows; c++)
+            {
+                res[r, c] = mat[c, r];
+            }
+        }
+        return res;
+    }
+}
