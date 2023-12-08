17f33932fe4472efa47c104c360a1b54f7841e3f
mshtang <32868278+mshtang@users.noreply.github.com>
2023-12-08 11:02:52 +0100

    WIP: PriorityQueue

---
+++ Solutions/Chapter2/Section0/PriorityQueue.cs
@@ -0,0 +1,44 @@
+namespace Solutions.Chapter2.Section0;
+
+public class PriorityQueue<T> where T : IComparable<T>
+{
+    public PriorityQueue()
+    {
+        // TODO
+    }
+
+    public PriorityQueue(int max)
+    {
+        // TODO
+    }
+
+    public PriorityQueue(T[] a)
+    {
+        // TODO
+    }
+
+    public void Insert(T v)
+    {
+        // TODO
+    }
+
+    public T DelMax()
+    {
+        // TODO
+    }
+
+    public T Max()
+    {
+
+    }
+
+    public bool IsEmpty()
+    {
+
+    }
+
+    public int Size()
+    {
+        return 0;
+    }
+}
